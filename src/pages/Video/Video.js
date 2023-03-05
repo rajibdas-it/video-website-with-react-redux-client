88845187ed4ba693656f509b5a50d80c8b961ef0
rajibdas-it <rajibrad@gmail.com>
2023-03-05 16:05:58 +0600

    added react router dom and setup outlet

---
+++ src/pages/Video/Video.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <div>
+      <h1>This is single Video page</h1>
+    </div>
+  );
+};
+
+export default Video;
---
+++ src/routes/route.js
@@ -0,0 +1,21 @@
+import { Children } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home";
+import Video from "../pages/Video/Video";
+import Videos from "../pages/Videos/Videos";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/videos", element: <Videos /> },
+      { path: "/video", element: <Video /> },
+    ],
+  },
+]);
+
+export default router;
