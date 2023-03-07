37f68bd41654169353c7cd5319bfe57c1c8bb2ac
rajibdas-it <rajibrad@gmail.com>
2023-03-07 13:40:01 +0600

    link added in loog

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import lws_logo from "../assets/img/lws.svg";
 import SearchForm from "./SearchForm";
 const Header = () => {
   return (
     <nav className="bg-slate-100 shadow-md">
       <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3">
-        <a href="/">
+        <Link to="/">
           <img className="h-10" src={lws_logo} alt="Learn with Sumit" />
-        </a>
+        </Link>
         <div className="border border-slate-200 flex items-center bg-white h-10 px-5 rounded-lg text-sm ring-emerald-200">
           <SearchForm />
         </div>
